0a10026c2c9d683bbdf7870dd5e8c4024b766d11
LeviHan <levihan@isystantapp.com>
2019-03-31 20:36:00 +0800

    去掉设置用户名和薪水

--- ICU996/ICU996/Module/Main/Controller/MainViewController.swift
+++ ICU996/ICU996/Module/Main/Controller/MainViewController.swift
@@ -60,15 +60,15 @@ class MainViewController: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let hasSetSalary = IcuCacheManager.get.hasSetSalary
-        if !hasSetSalary {
-            let startVc = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "ICUStartViewController")
-            present(startVc, animated: true, completion: nil)
-        }
-        //重新设置用户名
-        if let name = UserDefaults.standard.string(forKey: "username") {
-            headerView.setWelcomLabelText(name)
-        }
+//        let hasSetSalary = IcuCacheManager.get.hasSetSalary
+//        if !hasSetSalary {
+//            let startVc = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "ICUStartViewController")
+//            present(startVc, animated: true, completion: nil)
+//        }
+//        //重新设置用户名
+//        if let name = UserDefaults.standard.string(forKey: "username") {
+//            headerView.setWelcomLabelText(name)
+//        }
     }
     
     private func initialViews() {
